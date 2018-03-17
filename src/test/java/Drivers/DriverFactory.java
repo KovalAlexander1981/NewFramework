9bbf29b814a1ed28402931ecb382fe15ddb0c607
Koval Alex <trueno1@meta.ua>
2018-03-17 12:34:57 +0200

    New

--- src/test/java/Drivers/DriverFactory.java
+++ src/test/java/Drivers/DriverFactory.java
@@ -14,14 +14,14 @@ public class DriverFactory {
         String property = System.getProperty("driver");
         if ("firefox".equals(property)) {
             System.setProperty("webdriver.gecko.driver", "Driver/geckodriver.exe");
-            DriverFactory.driver = new FirefoxDriver();
+            driver = new FirefoxDriver();
         } else {
             System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
-            DriverFactory.driver = new ChromeDriver();
+           driver = new ChromeDriver();
         }
-        DriverFactory.driver.get(Constants.BASE_URL);
-        DriverFactory.driver.manage().window().maximize();
-        return DriverFactory.driver;
+      driver.get(Constants.BASE_URL);
+      driver.manage().window().maximize();
+        return driver;
 
     }
 }
