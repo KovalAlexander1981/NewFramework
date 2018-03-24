7eafe32600d817274e0abbe694200e4dcee3cad9
Koval Alex <trueno1@meta.ua>
2018-03-24 13:48:08 +0200

    new 18 mar3

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -2,12 +2,15 @@ package Pages;
 
 
 import Drivers.MyDriver;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 
 
 public class BasePage {
 
   public  static MyDriver driver = MyDriver.getMyDriver();
+    protected  Logger log = Logger.getLogger(this.getClass().getSimpleName());
+
 
     public BasePage(){
         PageFactory.initElements(driver, this);
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -14,12 +14,21 @@ public class LoginPage extends BasePage{
 	@FindBy(xpath = "//form[@name='lform']//input[@type='submit']")
 	private WebElement btnLogin;
 
+	@FindBy(name = "domn")
+	private WebElement domn;
+
+
 
     public void  login(String  name, String password){
+		log.info("set input name - " + name);
 		driver.jsSetValue(inputLogin, name);
-      //  inputLogin.sendKeys(name);
+       //inputLogin.sendKeys(name);
+		log.info("set input password - " + password);
         driver.jsSetValue(inputPassword, password);
 		//inputPassword.sendKeys(password);
+		//Select select = new Select(domn);
+		//select.selectByVisibleText("");
+
         btnLogin.click();
     }
 
